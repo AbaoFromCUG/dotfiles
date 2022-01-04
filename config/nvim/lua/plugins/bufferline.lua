2331332d0f9ce9f959fa455f64f5eaa21d2e0407
Abao <abaodoit@gmail.com>
2022-01-04 21:37:15 +0800

    bind ctrl-tab to cycle select tab

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -1,5 +1,7 @@
-return function ()
-    local bufferline = require("bufferline")
-    bufferline.setup{
-    }
+return function()
+    local bufferline = require "bufferline"
+    local opts = { noremap = true }
+    vim.api.nvim_set_keymap("n", "<c-c-i>", ":BufferLineCycleNext<CR>", opts)
+    vim.api.nvim_set_keymap("n", "<S-c-c-i>", ":BufferLineCycleNext<CR>", opts)
+    bufferline.setup {}
 end
