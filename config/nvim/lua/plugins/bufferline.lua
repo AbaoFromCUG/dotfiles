e0a60bc7a52ee0ae619e77a9b356eb4ee5986b09
Abao <abaodoit@gmail.com>
2022-01-05 22:36:20 +0800

    bufferline offset after NvTree

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -3,5 +3,18 @@ return function()
     local opts = { noremap = true }
     vim.api.nvim_set_keymap("n", "<c-c-i>", ":BufferLineCycleNext<CR>", opts)
     vim.api.nvim_set_keymap("n", "<S-c-c-i>", ":BufferLineCycleNext<CR>", opts)
-    bufferline.setup {}
+    bufferline.setup {
+        options = {
+            close_command = "bdelete! %d",
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "File Explorer",
+                    highlight = "Directory",
+                    text_align = "left",
+                },
+            },
+            separator_style = "slant",
+        },
+    }
 end
