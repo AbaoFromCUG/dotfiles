735e3c158c47ddda9b1e90296e042f9b6611e715
Abao <abaodoit@gmail.com>
2022-06-20 18:48:02 +0800

    config dapui new configuration

--- config/nvim/lua/lsp/sumneko_lua.lua
+++ config/nvim/lua/lsp/sumneko_lua.lua
@@ -1,13 +1,9 @@
 return function(config)
-    local runtime_path = vim.split(package.path, ";")
-    table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
     config.settings = {
         Lua = {
             runtime = {
                 -- LuaJIT in the case of Neovim
                 version = "LuaJIT",
-                path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
@@ -15,10 +11,11 @@ return function(config)
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                },
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
             },
         },
     }
