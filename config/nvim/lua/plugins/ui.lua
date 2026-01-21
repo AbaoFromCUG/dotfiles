e4415ee31e22ecbde4392d0392f885782d8f0013
Abao Zhang <abaodoit@gmail.com>
2026-01-22 00:22:22 +0800

    add translate-shell config and oil.nvim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -114,7 +114,7 @@ return {
     },
     {
         "willothy/flatten.nvim",
-        version = "0.5.1",
+        -- version = "0.5.1",
         lazy = false,
         priority = 1001,
         -- enabled = false,
@@ -329,6 +329,7 @@ return {
                             end,
                             desc = "yazi:current file",
                         },
+                        ["/"] = false
                     },
                 },
 
@@ -339,7 +340,31 @@ return {
             },
         },
     },
-
+    {
+        "stevearc/oil.nvim",
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {
+            default_file_explorer = true,
+            delete_to_trash = true,
+            skip_confirm_for_simple_edits = true,
+            view_options = {
+                show_hidden = true,
+                natural_order = true,
+                is_always_hidden = function(name, _)
+                    return name == ".." or name == ".git"
+                end,
+            },
+            win_options = {
+                wrap = true,
+            }
+        },
+        -- Optional dependencies
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+    },
     {
         "hedyhli/outline.nvim",
         opts = {},
