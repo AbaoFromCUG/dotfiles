5038ba94f52e6586b382d42308e8978bddf092af
Abao Zhang <abaodoit@gmail.com>
2026-02-06 14:28:20 +0800

    fix ssh clipboard paste behavior

--- config/nvim/plugin/clipboard.lua
+++ config/nvim/plugin/clipboard.lua
@@ -14,12 +14,6 @@ if vim.env.TMUX ~= nil and vim.env.SSH_TTY ~= nil then
         cache_enabled = 0,
     }
 elseif os.getenv("SSH_TTY") ~= nil then
-    local function no_paste(reg)
-        return function(lines)
-            -- Do nothing! We can't paste with OSC52
-        end
-    end
-
     vim.g.clipboard = {
         name = "OSC 52",
         copy = {
@@ -27,11 +21,8 @@ elseif os.getenv("SSH_TTY") ~= nil then
             ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
         },
         paste = {
-            -- ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-            -- ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-
-            ["+"] = no_paste("+"),
-            ["*"] = no_paste("*"),
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
         },
     }
 end
